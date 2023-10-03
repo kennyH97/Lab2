253aeda004a719a28c2ee0d4e53e5a90b684f2ff
kennyH97 <e.kennyh.v@gmail.com>
2023-10-03 15:32:16 +0700

    Add project

---
+++ Lab2/2.2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class _1
+    {
+        public void namXthangY()
+        {
+            int namX, thangY;
+            Console.Write("Nhap nam: ");
+            namX = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap thang: ");
+            bool namNhuan = true;
+
+            do
+            {
+                thangY = Convert.ToInt32(Console.ReadLine());
+            }
+            while (thangY < 1 || thangY > 12);
+
+            if (namX % 400 == 0 || (namX % 4 == 0 & namX % 100 != 0))
+            {
+                Console.WriteLine("Nam {0} la nam nhuan", namX);
+                namNhuan = true;
+            }
+            else
+            {    
+                Console.WriteLine("Nam {0} la nam khong nhuan", namX);  
+                namNhuan = false;
+            }
+
+            switch (thangY)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    Console.WriteLine("Thang {0} co 31 ngay.", thangY);
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    Console.WriteLine("Thang {0} co 31 ngay.", thangY);
+                    break;
+                case 2:
+                    if (namNhuan == true)
+                    {
+                        Console.WriteLine("Thang 2 co 29 ngay.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Thang 2 co 28 ngay.");
+                    }
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            _1 b1 = new _1();
+            b1.namXthangY();
+        }
+    }
+}
+
